fbc4ae5a1a3a7ae36e9a095364fe636fa03e6fe7
IOTBaicheng <30918679+IOTBaicheng@users.noreply.github.com>
2018-03-29 19:38:35 +0800

    remove unused files
    remove unused files

--- Huawei_LiteOS/targets/STM32F429IGTX_FIRE/MDK-ARM/Objects/arc4.d
+++
@@ -1,4 +0,0 @@
-.\objects\arc4.o: ..\..\..\components\security\mbedtls\mbedtls-2.6.0\library\arc4.c
-.\objects\arc4.o: ..\..\..\components\security\mbedtls\mbedtls_port\los_mbedtls_config.h
-.\objects\arc4.o: ..\..\..\components\security\mbedtls\mbedtls-2.6.0\include\mbedtls/check_config.h
-.\objects\arc4.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\limits.h
--- Huawei_LiteOS/targets/STM32F429IGTX_FIRE/MDK-ARM/Objects/oid.d
+++
@@ -1,4 +0,0 @@
-.\objects\oid.o: ..\..\..\components\security\mbedtls\mbedtls-2.6.0\library\oid.c
-.\objects\oid.o: ..\..\..\components\security\mbedtls\mbedtls_port\los_mbedtls_config.h
-.\objects\oid.o: ..\..\..\components\security\mbedtls\mbedtls-2.6.0\include\mbedtls/check_config.h
-.\objects\oid.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\limits.h
